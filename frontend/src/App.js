59ac1e6daab4dd663364d5a4cbc1dc13bde0b248
Your Name <you@example.com>
2022-12-18 14:03:18 -0700

    Obtained API Key, added .js file and modified .gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import { KEY } from "./localKey";
 
 // Pages Imports
 import HomePage from "./pages/HomePage/HomePage";
